9e3bbe13d9ed4b3fce284d4a8186b8bbd28cbc97
deeppatel51 <88723958+deeppatel51@users.noreply.github.com>
2022-04-09 18:14:58 +0530

    Create Code2.cpp

---
+++ Code2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Write your code here
+    int age;
+    string name;
+    cin >> name;
+    cin >> age;
+    cout << "The name of the person is " << name << " and the age is " << age << "." << endl;
+    return 0;
+}
