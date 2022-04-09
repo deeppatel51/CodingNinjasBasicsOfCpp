870d9e0668b11d98a4211e59d263c81b6e48fd9d
deeppatel51 <88723958+deeppatel51@users.noreply.github.com>
2022-04-09 18:35:16 +0530

    Create Code36.cpp

---
+++ Code36.cpp
@@ -0,0 +1,15 @@
+/*
+    Time Complexity: O(1)
+    Space Complexity: O(1)
+*/
+
+class Rectangle {
+
+public:
+int length, breadth;
+
+int getArea() {
+int area = length * breadth;
+return (area);
+}
+};
