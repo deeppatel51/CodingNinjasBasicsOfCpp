d5c5fb8c3f56d3a30323631388130c78460caabc
deeppatel51 <88723958+deeppatel51@users.noreply.github.com>
2022-04-09 18:14:09 +0530

    Create Code1.cpp

---
+++ Code1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Write your code here
+    int l, b;
+    cin >> l;
+    cin >> b;
+    int area = l*b;
+    cout << area << endl;
+
+    return 0;
+}
